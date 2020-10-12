e88574a8700d32b16aaa8a7d2e69a56e05dcfe15
Henric <henric.bjorkvall@gmail.com>
2020-10-12 11:24:54 +0200

    added basic authorization and fetch from spotify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
 import './App.css';
 
 function App() {
+  const url = `
+https://accounts.spotify.com/authorize?
+client_id=5d27d394dabe48ceb3546e56bc892ada&
+show_dialog=true&
+response_type=token&
+redirect_uri=http://localhost:3000`;
+
+  const getAccessToken = () => {
+    const parsed = queryString.parse(window.location.hash);
+
+    if (parsed.access_token !== undefined) {
+      return parsed.access_token;
+    }
+
+    return false;
+  };
+
+  const accessToken = getAccessToken();
+
+  useEffect(() => {
+    const rootUrl = 'https://api.spotify.com/v1';
+
+    fetch(`${rootUrl}/tracks/11dFghVXANMlKmJXsNCbNl`, {
+      headers: {
+        Authorization: 'Bearer ' + accessToken,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  }, [accessToken]);
+
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <a href={url} onClick={getAccessToken}>
+        Authorize
+      </a>
     </div>
   );
 }
