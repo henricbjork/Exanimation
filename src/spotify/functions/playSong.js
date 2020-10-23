f025303fed1eda3fd864e97a5062adfb10290192
Dominic Kersch <kerschdominic@gmail.com>
2020-10-23 16:35:48 +0200

    pause/play functionality

--- src/spotify/functions/playSong.js
+++ src/spotify/functions/playSong.js
@@ -1,10 +1,10 @@
-export const playSong = (uri, accessToken, device) => {
+export const playSong = (uri, accessToken, device, position=0) => {
   if (!accessToken) return;
   return new Promise(function (resolve, reject) {
     const rootUrl = 'https://api.spotify.com/v1';
     fetch(`${rootUrl}/me/player/play?device_id=${device}`, {
       method: 'PUT',
-      body: JSON.stringify({ uris: [`${uri}`] }),
+      body: JSON.stringify({ uris: [`${uri}`], position_ms: `${position}` }),
       headers: {
         Authorization: 'Bearer ' + accessToken,
       },
