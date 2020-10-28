acfa4863b335b18160893649a4e6f472fa4d91dd
Henric <henric.bjorkvall@gmail.com>
2020-10-28 13:10:57 +0100

    added three-tube-wireframe package. this is used to make the wireframe more customizable in the icosahedron component. the lines are now thicker

--- src/spotify/functions/getRecommendations.js
+++ src/spotify/functions/getRecommendations.js
@@ -2,7 +2,7 @@ export const getRecommendations = (accessToken, id) => {
   if (!accessToken) return;
   return new Promise(function (resolve, reject) {
     const rootUrl = 'https://api.spotify.com/v1';
-    fetch(`${rootUrl}/recommendations?seed_tracks=${id}&limit=42`, {
+    fetch(`${rootUrl}/recommendations?seed_tracks=${id}&limit=12`, {
       headers: {
         Authorization: 'Bearer ' + accessToken,
       },
--- src/threefiber/components/Icosahedron.js
+++ src/threefiber/components/Icosahedron.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as THREE from 'three';
 import { useSpring, a } from 'react-spring';
 import Song from './Song';
+import createTubeWireframe from 'three-tube-wireframe';
 
 const Icosahedron = ({
   recommendations,
@@ -19,14 +20,22 @@ const Icosahedron = ({
     SIZE = 8;
   }
 
-  const vertices = new THREE.IcosahedronGeometry(SIZE, 1).vertices;
+  const geo = new THREE.IcosahedronGeometry();
+
+  const wireGeo = createTubeWireframe(geo, {
+    thickness: 0.01, // thickness in world units of tubes
+    radiusSegments: 4, // number of segments around the tubes
+    mode: 'triangle', // face layout, use quads instead of triangles
+  });
+
+  const vertices = new THREE.IcosahedronGeometry(SIZE).vertices;
 
   return (
     <>
-      <mesh scale={[SIZE, SIZE, SIZE]}>
-        <icosahedronBufferGeometry attach='geometry' args={[1, 1]} />
-        <meshBasicMaterial attach='material' wireframe />
+      <mesh scale={[SIZE, SIZE, SIZE]} geometry={wireGeo}>
+        <meshPhysicalMaterial attach='material' roughness='0.75' flatShading />
       </mesh>
+
       {recommendations &&
         recommendations.map((recommendation, i) => {
           return (
