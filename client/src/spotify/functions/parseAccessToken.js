841723698036e40990f80d7a0890319e126fd25e
Dominic Kersch <kerschdominic@gmail.com>
2020-11-09 14:04:34 +0100

    hide access token from url

--- client/src/spotify/functions/parseAccessToken.js
+++ client/src/spotify/functions/parseAccessToken.js
@@ -1,9 +1,14 @@
 import queryString from 'query-string';
 
 export const parseAccessToken = () => {
+  if (sessionStorage.getItem('accessToken')!==null) {
+    return sessionStorage.getItem('accessToken');
+  }
   const parsed = queryString.parse(window.location.hash);
   if (parsed.access_token !== undefined) {
-    return parsed.access_token;
+    sessionStorage.setItem('accessToken', parsed.access_token);
+    window.history.pushState({}, null, "http://localhost:3000/");
+    return sessionStorage.getItem('accessToken');
   }
   return false;
 };
