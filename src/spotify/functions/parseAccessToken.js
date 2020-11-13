84e41059186d7c7688ffd826949b81539ca143ac
Dominic Kersch <kerschdominic@gmail.com>
2020-11-13 13:34:33 +0100

    fix pushState in parseAccessToken function

--- src/spotify/functions/parseAccessToken.js
+++ src/spotify/functions/parseAccessToken.js
@@ -1,4 +1,5 @@
 import queryString from 'query-string';
+const backend_endpoint = process.env.REACT_APP_BACKEND_ENDPOINT;
 
 export const parseAccessToken = () => {
   if (sessionStorage.getItem('accessToken')!==null) {
@@ -7,7 +8,7 @@ export const parseAccessToken = () => {
   const parsed = queryString.parse(window.location.hash);
   if (parsed.access_token !== undefined) {
     sessionStorage.setItem('accessToken', parsed.access_token);
-    window.history.pushState({}, null, "http://localhost:3000/");
+    window.history.pushState({}, null, backend_endpoint);
     return sessionStorage.getItem('accessToken');
   }
   return false;
