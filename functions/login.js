e37bca4655108b558932501bb89efc8768e8d2ea
Henric <henric.bjorkvall@gmail.com>
2020-11-17 15:33:23 +0100

    made title text smaller and changed login window padding

--- functions/login.js
+++ functions/login.js
@@ -4,22 +4,20 @@ process.env.SILENCE_EMPTY_LAMBDA_WARNING = true; // removes a netlify-lambda war
 const client_id = process.env.SPOTIFY_CLIENT;
 const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
 
-exports.handler = function(event, context, callback) {
-  const scope = 'user-read-private%20user-read-email%20user-modify-playback-state%20user-read-playback-state%20user-read-recently-played';
+exports.handler = function (event, context, callback) {
+  const scope =
+    'user-read-private%20user-read-email%20user-modify-playback-state%20user-read-playback-state%20user-read-recently-played';
   const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}`;
 
   try {
-    setTimeout(() => {
-      return callback(null,{
-        statusCode: 301,
-        headers: {
-          Location: url,
-          "Cache-Control": "no-cache"
-        }
-      })
-    }, 1000);
+    return callback(null, {
+      statusCode: 301,
+      headers: {
+        Location: url,
+        'Cache-Control': 'no-cache',
+      },
+    });
   } catch (err) {
-    return callback(null,{ statusCode: 500, body: err.message });
+    return callback(null, { statusCode: 500, body: err.message });
   }
-
-}
+};
