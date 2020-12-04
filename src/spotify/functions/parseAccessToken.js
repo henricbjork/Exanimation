a66ec503d9a059534071fa5cb6cc70a965d3dff2
Dominic Kersch <kerschdominic@gmail.com>
2020-12-04 22:41:37 +0100

    remove console log

--- src/spotify/functions/parseAccessToken.js
+++ src/spotify/functions/parseAccessToken.js
@@ -2,14 +2,13 @@ import queryString from 'query-string';
 const base_url = process.env.REACT_APP_BASE_URL;
 
 export const parseAccessToken = () => {
-  if (sessionStorage.getItem('accessToken')!==null) {
+  if (sessionStorage.getItem('accessToken') !== null) {
     window.history.pushState({}, null, base_url);
     return sessionStorage.getItem('accessToken');
   }
   const parsed = queryString.parse(window.location.hash);
   if (parsed.access_token !== undefined) {
     sessionStorage.setItem('accessToken', parsed.access_token);
-    console.log('dean')
     window.location.href = base_url;
     return sessionStorage.getItem('accessToken');
   }
